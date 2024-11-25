fa98af4cecf9db2310956f43f64ca37625444d5d
adoumasseo <adoumasseo@gmail.com>
2024-11-25 07:32:03 -0800

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,23 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+"""script that provides some stats about Nginx logs stored in MongoDB"""
+
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
-
-
-if __name__ == '__main__':
-    run()
+nginx_collection = MongoClient().logs.nginx
+logs_count = nginx_collection.count_documents({})
+GET_count = nginx_collection.count_documents({'method': 'GET'})
+POST_count = nginx_collection.count_documents({'method': 'POST'})
+PUT_count = nginx_collection.count_documents({'method': 'PUT'})
+PATCH_count = nginx_collection.count_documents({'method': 'PATCH'})
+DELETE_count = nginx_collection.count_documents({'method': 'DELETE'})
+GET_STATUS = nginx_collection.count_documents(
+        {'method': 'GET', 'path': "/status"})
+print(
+        "{} logs\nMethods:\n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod"
+        " PUT: {}\n\tmethod PATCH: {}\n\tmethod DELETE: {}\n{} "
+        "status check".format(logs_count, GET_count, POST_count,
+                              PUT_count, PATCH_count, DELETE_count,
+                              GET_STATUS
+                              )
+    )
