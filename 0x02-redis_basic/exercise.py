835973a30865295ed4e45c36b0d0490e5780b897
adoumasseo <adoumasseo@gmail.com>
2024-11-27 12:18:50 +0100

    Adding a get method to retrive some data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """First task of using Redis"""
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Any
 
 
 class Cache:
@@ -18,3 +18,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        '''Retrieves a value from a redis database'''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> Union[str, bytes, int, float]:
+        '''Retrieves a string value from a Redis data storage.'''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[str, bytes, int, float]:
+        '''Retrieves a int value from a Redis data storage'''
+        return self.get(key, lambda x: int(x))
